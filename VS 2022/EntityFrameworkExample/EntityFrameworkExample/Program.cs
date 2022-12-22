184ab2f23ca56d998dc0186d4a9c8cf6a498b1a7
Danpier Dos Reis <danpier_dr@hotmail.com>
2022-12-22 12:42:50 -0300

    Ejemplo conexion a Db con Entity Framework

---
+++ VS 2022/EntityFrameworkExample/EntityFrameworkExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new TaskyAppEntities()){
+
+                                                                                     //Le puse 'hola' pero
+                                                                                     //puede ser cualquier
+                                                                                     //nombre
+                var listadatos = contexto.Areas.Where(hola =>hola.Id>4);
+                                                                                //Donde Id sea mayor a 4
+                foreach (var ormEntidad in listadatos){
+
+                    Console.WriteLine("{0}--{1}--{2}",
+                        ormEntidad.Id, ormEntidad.Area1, ormEntidad.Fecha);
+                    Console.ReadKey();
+
+                }
+
+            }
+        }
+    }
+}
