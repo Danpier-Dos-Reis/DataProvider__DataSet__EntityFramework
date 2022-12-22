455b60d55fee97d98457aa4dd43f60b0dbead895
Danpier Dos Reis <danpier_dr@hotmail.com>
2022-12-22 00:09:14 -0300

    Ejemplo de coneccion a DB con el proveedor que de la librería System.Data.SqlClient

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data.SqlClient;
+
+string connectionstring = @"Data Source=DANPIER2022; Initial Catalog=TaskyApp;
+                            User id=sa; Password=th16g0;";
+string query = "SELECT * FROM Areas";
+
+using (SqlConnection conection = new SqlConnection(connectionstring)){
+    SqlCommand command = new SqlCommand(query,conection);
+    try
+    {
+        conection.Open();
+        SqlDataReader reader = command.ExecuteReader();
+        while(reader.Read()){       /*reader[0] = Primera Columna ; reader[1] = Segunda Columna*/
+            Console.WriteLine("{0} -- {1}",reader[0],reader[1]);
+        }
+        conection.Close();
+    }
+    catch (Exception error)
+    {
+        Console.WriteLine(error.Message);
+    }
+}
