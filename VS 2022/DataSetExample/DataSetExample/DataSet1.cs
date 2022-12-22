5f1bd26aaa23eec8d4b9ab8757cea615e5b43cbb
Danpier Dos Reis <danpier_dr@hotmail.com>
2022-12-22 02:08:44 -0300

    Ejemplo de coneccion a la Db con DataSet

---
+++ VS 2022/DataSetExample/DataSetExample/DataSet1.cs
@@ -0,0 +1,38 @@
+﻿namespace DataSetExample
+{
+}
+
+namespace DataSetExample
+{
+}
+
+namespace DataSetExample
+{
+}
+
+namespace DataSetExample
+{
+}
+
+namespace DataSetExample
+{
+}
+
+namespace DataSetExample
+{
+}
+
+namespace DataSetExample
+{
+}
+
+namespace DataSetExample
+{
+}
+
+namespace DataSetExample.DataSet1TableAdapters {
+    
+    
+    public partial class AreasTableAdapter {
+    }
+}
